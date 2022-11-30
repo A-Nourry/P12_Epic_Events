cedfdac74091fa41069c732ffa9bdb18f09e08d8
Andrew Nourry <andrewnourry@gmail.com>
2022-11-30 18:51:25 +0100

    initial commit

---
+++ myapp/management/commands/init_local_dev.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+ADMIN_ID = "admin"
+ADMIN_PASSWORD = "admin"
+
+
+class Command(BaseCommand):
+
+    help = "Initialize project for local development"
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.MIGRATE_HEADING(self.help))
+
+        UserModel.objects.create_superuser(
+            ADMIN_ID, "admin@dev.com", ADMIN_PASSWORD
+        )
+
+        self.stdout.write(self.style.SUCCESS("All Done !"))
