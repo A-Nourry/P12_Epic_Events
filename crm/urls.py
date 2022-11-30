216f3a38d60e75d86f642d9bc1fa583352ce658b
Andrew Nourry <andrewnourry@gmail.com>
2022-11-30 18:51:53 +0100

    login, signup path added

--- crm/urls.py
+++ crm/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from authentication.views import SignupViewset, UserViewset
+
+router = routers.SimpleRouter()
+router.register("signup", SignupViewset, basename="signup")
+router.register("users", UserViewset, basename="users")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
