27724f47f658d3110ad2ab798801ad5ed95c0d0e
Andrew Nourry <andrewnourry@gmail.com>
2022-12-02 18:55:18 +0100

    initial commit

---
+++ myapp/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Client
+
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "mobile",
+            "company_name",
+            "date_created",
+            "date_updated",
+            "sales_contact",
+        ]
