720d9c2ca6d80d7e063dc431dc55fc3472f1181a
Andrew Nourry <andrewnourry@gmail.com>
2022-12-02 18:55:29 +0100

    AUTH_USER_MODEL added

--- crm/settings.py
+++ crm/settings.py
@@ -141,6 +141,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "authentication.Employee"
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
