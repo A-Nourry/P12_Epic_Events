ad2219f3897c678d30dd126a77a158d23a37ecd8
Andrew Nourry <andrewnourry@gmail.com>
2022-12-02 18:55:46 +0100

    Client admin display added

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "last_name",
+        "first_name",
+        "email",
+        "company_name",
+        "date_created",
+        "date_updated",
+        "sales_contact",
+    )
+
+
+admin.site.register(Client, ClientAdmin)
