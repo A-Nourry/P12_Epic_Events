e5223197ec7a8e01260a54df7a48208fd8a5b929
Andrew Nourry <andrewnourry@gmail.com>
2022-12-05 10:45:58 +0100

    event view added

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Client, Contract
+from .models import Client, Contract, Event
 
 
 class ClientAdmin(admin.ModelAdmin):
@@ -29,5 +29,21 @@ class ContractAdmin(admin.ModelAdmin):
     )
 
 
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = (
+            "id",
+            "client",
+            "date_created",
+            "date_updated",
+            "support_contact",
+            "event_status",
+            "attendees",
+            "event_date",
+            "notes",
+        )
+
+
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Contract, ContractAdmin)
+admin.site.register(Event, EventAdmin)
