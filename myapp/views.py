5060bd40a6a4c902975f683415b03eb13e1a5f07
Andrew Nourry <andrewnourry@gmail.com>
2022-12-07 16:29:52 +0100

    permissions and filters added to all classes

--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,63 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import PermissionDenied
 
 from .models import Client, Contract, Event
+from authentication.permissions import IsManager, IsSupport, IsSeller
 from .serializers import ClientSerializer, ContractSerializer, EventSerializer
 
 
 class ClientViewset(ModelViewSet):
     serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated & IsManager | IsSeller | IsSupport]
 
     def get_queryset(self):
-        return Client.objects.all()
+        user = self.request.user
+        event = Event.objects.get(support_contact=user)
+
+        if user.user_team == "MANAGEMENT":
+            return Client.objects.all()
+
+        if user.user_team == "SALES":
+            return Client.objects.filter(sales_contact=user)
+
+        if user.user_team == "SUPPORT":
+            return Client.objects.filter(id=event.client.id)
+
+        raise PermissionDenied
 
 
 class ContractViewset(ModelViewSet):
     serializer_class = ContractSerializer
+    permission_classes = [IsAuthenticated & IsManager | IsSeller]
 
     def get_queryset(self):
-        return Contract.objects.all()
+        user = self.request.user
+
+        if user.user_team == 'MANAGEMENT':
+            return Contract.objects.all()
+
+        if user.user_team == 'SALES':
+            return Contract.objects.filter(sales_contact=user)
+
+        raise PermissionDenied
 
 
 class EventViewset(ModelViewSet):
     serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated & IsManager | IsSeller | IsSupport]
 
     def get_queryset(self):
-        return Event.objects.all()
+        user = self.request.user
+        client = Client.objects.filter(sales_contact=user)
+
+        if user.user_team == 'MANAGEMENT':
+            return Event.objects.all()
+
+        if user.user_team == "SALES":
+            return Event.objects.filter(client__in=client)
+
+        if user.user_team == 'SUPPORT':
+            return Event.objects.filter(support_contact=user)
+
+        raise PermissionDenied
