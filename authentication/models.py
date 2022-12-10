b627a070f3e5d6cfa8718a3390a4cf157243d232
Andrew Nourry <andrewnourry@gmail.com>
2022-12-10 15:24:39 +0100

    Normal user are no longer superusers by default

--- authentication/models.py
+++ authentication/models.py
@@ -71,10 +71,10 @@ class Employee(AbstractBaseUser):
         verbose_name="équipe",
     )
 
-    is_admin = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     objects = MyAccountManager()
